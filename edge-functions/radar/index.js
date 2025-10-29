bb2564fe95a2167814a3fd0f318e9955689242f2
yms <ymslucky@163.com>
2025-10-29 09:23:45 +0800

    fix(edge-functions): 修复数据返回和日志输出问题

--- edge-functions/radar/index.js
+++ edge-functions/radar/index.js
@@ -1,7 +1,8 @@
+
 export async function onRequest(context) {
     const data = await date_view.get('test1');
-    console.log(data.json);
-    return new Response(JSON.stringify(data.json), {
+    console.log(data);
+    return new Response(JSON.stringify(data), {
         headers: {
             'content-type': 'application/json',
             'x-edgefunctions': 'Welcome to use EdgeOne Pages Functions.',
